bc252d967693533fde93a76a2e810a7c1d613e4d
nicolaspenagos <nicolas.penagosm98@gmail.com>
2023-07-25 15:40:43 -0500

    Replace Redux with custom hooks

---
+++ exercise-21-replace-redux-with-react-hooks/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import ProductsProvider from "./context/products-context";
+import configureStore from "./hooks-store/products-store";
+import "./index.css";
+import App from "./App";
+
+
+configureStore();
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+
+);
